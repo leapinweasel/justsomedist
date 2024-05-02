277c2ba5447f009535f8d6e06ca340dde3d26e27
lhinz <levi.hinz@arcadis.com>
2024-05-02 16:41:56 +0100

    pages

--- script.js
+++ script.js
@@ -29,3 +29,12 @@ function generateRandomWords() {
             console.error('Error fetching word list:', error);
         });
 }
+
+// Function to shuffle an array
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
